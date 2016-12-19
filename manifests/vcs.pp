590e2bc32cad2f57b067bf7f86918c7a052d6d9c
Eric Sorenson <eric.sorenson@puppet.com>
2016-12-19 13:38:51 -0800

    vcs.pp derived from configure.pp

--- manifests/vcs.pp
+++ manifests/vcs.pp
@@ -1,86 +1,48 @@
-# Class veritas::vcs
-#
-class veritas::vcs (
-  $lltoverudp =      $::veritas::vcs::params::lltoverudp,
-  $installrecpkgs =  $::veritas::vcs::params::installrecpkgs,
-  $updatkeys =       $::veritas::vcs::params::updatekeys,
-  $vxkeyless =       $::veritas::vcs::params::vxkeyless,
-  $prod =            $::veritas::vcs::params::prod,
-  $smtprsev =        $::veritas::vcs::params::smtprsev,
-  $updatekeys =      $::veritas::vcs::params::updatekeys,
-  $allowcomms =      $::veritas::vcs::params::allowcomms,
-  $eat_security =    $::veritas::vcs::params::eat_security,
-  $answer_file =     $::veritas::vcs::params::answer_file,
-  $install_script =  $::veritas::vcs::params::install_script,
-  $eat_security=     $::veritas::vcs::params::eat_security,
-  $systems =         [],
-  $heartbeat_links = [],
-  $lopri_link =      '',
-  $nodename =        $::hostname,
-  $clusterid=        undef,
-  $clustername,
-  $csgnetmask=       '255.255.255.0',
-  $csgnic,
-  $csgvip,
-  $smtprecp,
-  $smtpserver,
-  $eat_security_fips = undef,
-  $gconetmask =      undef,
-  $gconic =          undef,
-  $gcovip =          undef,
-  $securegco =       undef,
-  $smtpserv =        undef,
-  $snmpcons =        undef,
-  $snmpcsev =        undef,
-  $snmpport =        undef,
-  $userenpw =        undef,
-  $username =        undef,
-  $userpriv =        undef,
-) inherits veritas::vcs::params {
+class veritas_infoscale::configure (
+	$responsefile =		'/tmp/configure.response',
+	$install_script =	'/opt/VRTS/install/installer',
+	$prod =				'AVAILABILITY72',
+	$activecomponent =	'VCS72',
+	$keyless =			'AVAILABILITY',
+	$lltoverudp =		'0',
+	$smtprsev =			undef,
+	$allowcomms =		'1',
+	$eat_security =		'1',
+	$systems =			[ $::hostname ],
+	$heartbeat_links =	[],
+	$lopri_link =		'',
+	$clusterid =		undef,
+	$clustername,
+	$csgnetmask =      undef,
+	$csgnic =          undef,
+	$csgvip =          undef,
+	$smtprecp =        undef,
+	$smtpserver =      undef,
+	$eat_security_fips = undef,
+	$gconetmask =      undef,
+	$gconic =          undef,
+	$gcovip =          undef,
+	$securegco =       undef,
+	$smtpserv =        undef,
+	$snmpcons =        undef,
+	$snmpcsev =        undef,
+	$snmpport =        undef,
+	$userenpw =        undef,
+	$username =        undef,
+	$userpriv =        undef,
 
-  include veritas
+) {
 
-  if count($systems) > 0 {
-    if (count($heartbeat_links) > 0) or $lopri_link {
-      fail('Must use *one* of systems[] or heartbeat_links and lopri_link - see "mode" documentation')
-      $target_systems=$systems
-    }
- } else {
-    if (count($heartbeat_links) < 1) or !$lopri_link {
-      fail('If the systems[] parameter is not used then heartbeat_links[] and lopri_link *must* be set')
-    }
-    $target_systems = [ {
-      "${nodename}" => {
-        'heartbeat_links' => $heartbeat_links,
-        'lopri_link' => $lopri_link
-      }
-    } ]
-  }
-
-  include veritas::packages
-  file { $answer_file:
-    ensure => file,
-    content => template('veritas/vcs_responses.erb'),
-  }
-
-  Package <| title == 'VRTSperl' |> ->
-  Package <| title == 'VRTSvlic' |> ->
-  Package <| title == 'VRTSspt' |> ->
-  Package <| title == 'VRTSllt' |> ->
-  Package <| title == 'VRTSgab' |> ->
-  Package <| title == 'VRTSvxfen' |> ->
-  Package <| title == 'VRTSamf' |> ->
-  Package <| title == 'VRTSvcs' |> ->
-  Package <| title == 'VRTScps' |> ->
-  Package <| title == 'VRTSvcsag' |> ->
-  Package <| title == 'VRTSvcsdr' |> ->
-  Package <| title == 'VRTSvcsea' |> ->
-  Package <| title == 'VRTSsfmh' |> ->
-  Package <| title == 'VRTSvbs' |> ->
-  Package <| title == 'VRTSvcswiz' |> ->
-  Package <| title == 'VRTSsfcpi62' |>->
-  class { 'veritas::vcs::install': }->
-  class { 'veritas::vcs::service': }
+	file { $responsefile:
+		ensure => file,
+		content => template('veritas_infoscale/configure.response.erb'),
+	}
+	->
+	exec { 'veritas_infoscale::configure':
+		command => "${install_script} -responsefile ${responsefile}",
+		timeout => 0,
+		logoutput => true,
+		unless => '/opt/VRTS/bin/hastatus -sum',
+	}
 
 }
-
